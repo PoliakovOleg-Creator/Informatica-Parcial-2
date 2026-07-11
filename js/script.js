51bc1a592bc5e5b5d817f205b50e2b445fec5125
PoliakovOleg-Creator <oleglive13@gmail.com>
2026-07-11 13:14:48 -0300

    corr2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // --- 1. DATOS CURIOSOS (Math.random) ---
 // ACA USE IA: Me ayudó a entender cómo usar Math.floor para que el índice no de error
 const listaDatos = [
-    "Rafael Lozano-Hemmer nació en México y estudió Química Física [3].",
-    "Su obra utiliza sensores biométricos para captar latidos del corazón [4].",
-    "Ha expuesto sus obras en más de 70 países [5].",
-    "Ganó el premio Ars Electronica por su innovación [5].",
-    "Sus instalaciones necesitan al público para existir [6]."
+    "Rafael Lozano-Hemmer nació en México y estudió Química Física.",
+    "Su obra utiliza sensores biométricos para captar latidos del corazón.",
+    "Ha expuesto sus obras en más de 70 países.",
+    "Ganó el premio Ars Electronica por su innovación.",
+    "Sus instalaciones necesitan al público para existir."
 ];
 
 document.querySelector('#btn-dato').addEventListener('click', function() {
